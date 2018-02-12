e3de1c93368e25f764b7334fdf61fe9deec9866f
SDatsyuk <35565131+SDatsyuk@users.noreply.github.com>
2018-02-12 17:23:25 +0200

    Update output.py

--- output.py
+++ output.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ciga_main.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
 import os
 import sys
 import json
